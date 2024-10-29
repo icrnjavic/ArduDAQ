4ffd6c98cdf1549f00b5787bc3b1f7967f6ea6e0
icrnjavic <32548477+icrnjavic@users.noreply.github.com>
2024-10-29 22:18:27 +0100

    added shield digital output support
    - D0 and D1 dont work as digital outputs since they are Tx and Rx.
    -> For V2 D0 and D1 wont be used as digital output pins.

--- main/main.ino
+++ main/main.ino
@@ -12,7 +12,7 @@
 
 Adafruit_ADS1115 ads;
 
-//array of voltage resistor values for individual channels 1-4 from left to right
+// array of voltage resistor values for individual channels 1-4 from left to right
 float R1[4] = {10000.0, 10000.0, 10000.0, 10000.0};
 float R2[4] = {1000.0, 1000.0, 1000.0, 1000.0};
 
@@ -28,27 +28,40 @@ float currentDeadband = 0.05;  // deadband noise filter
 
 bool continuousMode = false;
 unsigned long lastMeasurementTime = 0;
-const unsigned long measurementInterval = 10; // interval
+const unsigned long measurementInterval = 10;
+
+// check if its a valid ads1115 channel(0-3)
+bool isValidChannel(int channel) {
+  return (channel >= 0 && channel < 4);
+}
 
 void setup() {
   Serial.begin(115200);
-  // initialize modules
   if (!ads.begin()) {
     Serial.println("Failed to initialize ADS1115!");
     while (1);
   }
   //Serial.println("ADS1115 initialized.");
-  sensors.begin();
   //Serial.println("DS18B20 initialized.");
+  sensors.begin();
 
   // set shield pins as outputs
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
+  pinMode(0, OUTPUT); // wont work due to it being a serial pin -> usable for Tx/Rx usage
+  pinMode(1, OUTPUT); // wont work due to it being a serial pin -> usable for Tx/Rx usage
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
+
+  // set all output pins to LOW by default
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
 
   // calibrate acs712 offset
   calibrateACS712Offset();
@@ -67,51 +80,73 @@ void loop() {
 }
 
 void processCommand(String command) {
-  if (command == "START_CONTINUOUS") {
-    continuousMode = true;
-    Serial.println("Continuous mode started");
-  } else if (command == "STOP_CONTINUOUS") {
-    continuousMode = false;
-    Serial.println("Continuous mode stopped");
-  } else if (command == "READ_CHANNEL_1") {
-    Serial.print("Channel 0 Voltage: ");
-    Serial.println(readChannelVoltage(0));
-  } else if (command == "READ_CHANNEL_2") {
-    Serial.print("Channel 1 Voltage: ");
-    Serial.println(readChannelVoltage(1));
-  } else if (command == "READ_CHANNEL_3") {
-    Serial.print("Channel 2 Voltage: ");
-    Serial.println(readChannelVoltage(2));
-  } else if (command == "READ_CHANNEL_4") {
-    Serial.print("Channel 3 Voltage: ");
-    Serial.println(readChannelVoltage(3));
-  } else if (command == "READ_CURRENT") {
-    Serial.print("Averaged Current (ACS712): ");
-    float averagedCurrent = readCurrentAverage();
-    
-    // deadband filter (WIP)
-    if (abs(averagedCurrent) < currentDeadband) {
-      averagedCurrent = 0.0;
-    }
-    Serial.print(averagedCurrent);
-    Serial.println(" A");
-  } else if (command == "READ_TEMPERATURE") {
-    Serial.print("Temperature (DS18B20): ");
-    Serial.print(readTemperature());
-    Serial.println(" °C");
-  } else if (command.startsWith("SET_PIN_")) {
-    int pin = command.substring(8, 10).toInt();
-    int state = command.substring(11).toInt();
+  if (command.startsWith("SET_")) {
+    int pin = command.charAt(4) - '0'; // extract the pin number    
     if (isValidPin(pin)) {
-      digitalWrite(pin, state);
-      Serial.print("Pin D");
-      Serial.print(pin);
-      Serial.print(" set to ");
-      Serial.println(state ? "HIGH" : "LOW");
+      if (command.endsWith("_ON")) {
+        digitalWrite(pin, HIGH); // set extracted pin to HIGH
+        Serial.print("Pin D");
+        Serial.print(pin);
+        Serial.println(" set to HIGH");
+      } else if (command.endsWith("_OFF")) {
+        digitalWrite(pin, LOW); // set the extracted pin to LOW
+        Serial.print("Pin D");
+        Serial.print(pin);
+        Serial.println(" set to LOW");
+      } else {
+        Serial.println("Invalid command");
+      }
     } else {
       Serial.println("Invalid pin");
     }
-  } 
+  } else if (command.equals("info")) {
+    Serial.println("SET_<pin_number>_ON - Sets the specified pin to HIGH.");
+    Serial.println("SET_<pin_number>_OFF - Sets the specified pin to LOW.");
+    Serial.println("READ_CHANNEL_<channel_number> - Reads the voltage of the specified channel(1-4).");
+    Serial.println("READ_TEMPERATURE - Reads the temperature from the onboard sensor.");
+    Serial.println("START_CONTINUOUS - Starts continuous measurements.");
+    Serial.println("STOP_CONTINUOUS - Stops continuous measurements.");
+    Serial.println("READ_CURRENT - Reads the current from the single channel.");
+  } else if (command.equals("START_CONTINUOUS")) {
+    continuousMode = true;
+    Serial.println("Continuous mode started");
+  } else if (command.equals("STOP_CONTINUOUS")) {
+    continuousMode = false;
+    Serial.println("Continuous mode stopped");
+  } else if (command.equals("READ_CHANNEL_1")) {
+    Serial.print("Channel 0 Voltage: ");
+    Serial.println(readChannelVoltage(0));
+  } else if (command.equals("READ_CHANNEL_2")) {
+    Serial.print("Channel 1 Voltage: ");
+    Serial.println(readChannelVoltage(1));
+  } else if (command.equals("READ_CHANNEL_3")) {
+    Serial.print("Channel 2 Voltage: ");
+    Serial.println(readChannelVoltage(2));
+  } else if (command.equals("READ_CHANNEL_4")) {
+    Serial.print("Channel 3 Voltage: ");
+    Serial.println(readChannelVoltage(3));
+  } else if (command.equals("READ_CURRENT")) {
+    Serial.print("Averaged Current (ACS712): ");
+    float averagedCurrent = readCurrentAverage();
+    Serial.println(averagedCurrent);
+  } else if (command.startsWith("READ_CHANNEL_")) {
+    int channel = command.charAt(13) - '0'; // Extract channel number
+    if (isValidChannel(channel)) {
+      float voltage = readChannelVoltage(channel);
+      Serial.print("Channel ");
+      Serial.print(channel);
+      Serial.print(": ");
+      Serial.print(voltage);
+      Serial.println(" V");
+    } else {
+      Serial.println("Invalid channel");
+    }
+  } else if (command.equals("READ_TEMPERATURE")) {
+    float temperature = readTemperature();
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
+  }
 }
 
 void sendContinuousMeasurements() {
@@ -161,7 +196,7 @@ float readCurrentAverage() {
 // read current from acs712 via A3 pin
 float readCurrent() {
   int16_t currentAdcReading = analogRead(A3); 
-  float currentVout = (currentAdcReading * 5.0) / 1023.0;  // TO DO: for V2 use ads1115 instead of internal adc for better resolution
+  float currentVout = (currentAdcReading * 5.0) / 1023.0;  // TO DO: for V2 use a second ads1115 instead of internal adc for better resolution
   return (currentVout - ACS712_Offset) / ACS712_Sensitivity;
 }
 
@@ -187,7 +222,7 @@ float readTemperature() {
   return sensors.getTempCByIndex(0);
 }
 
-// check if the pin is one of the valid shield pins
+// check if the selected output pin is one of the valid shield pins
 bool isValidPin(int pin) {
-  return (pin == 0 || pin == 1 || (pin >= 3 && pin <= 7));
+  return (pin == 0 || pin == 1 || pin == 3 || pin == 4 || pin == 5 || pin == 6 || pin == 7);
 }
